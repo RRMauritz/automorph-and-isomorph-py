346a1f8b925df6c817d10f8f7cc2c30653bf2c8c
s1862294 <s1862294@utwente.nl>
2019-02-27 19:15:15 +0100

    Test

--- color_refinement.py
+++ color_refinement.py
@@ -1,11 +1,13 @@
 from graph import *
+from graph_io import load_graph, write_dot
 from collections import Counter
 
-def color_refinement(graph: "Graph"):
-    vertices = graph.vertices
 
-    colors = {v: v.degree for v in vertices}    # initialization
-    last_color = max([c for k, c in colors])
+def color_refinement(graph: "Graph"):
+    vert = graph.vertices
+
+    colors = {v: v.degree for v in vert}    # initialization
+    last_color = max([c for k, c in colors.items()])
     colors_old = {}
 
     while not colors == colors_old:
@@ -13,8 +15,8 @@ def color_refinement(graph: "Graph"):
 
         # create coloring of vertices
 
-        for u in vertices:
-            for v in vertices:
+        for u in vert:
+            for v in vert:
                 if u == v:
                     continue
                 if colors_old[u] == colors_old[v]:
@@ -22,13 +24,29 @@ def color_refinement(graph: "Graph"):
                     v_color_neighbourhood = Counter([colors_old[k] for k in v.neighbours])
                     if u_color_neighbourhood == v_color_neighbourhood:
                         last_color += 1
-                        colors[u], colors[v] = last_color
+                        colors[u] = last_color
+                        colors[v] = last_color
                     else:
                         last_color += 1
                         colors[u] = last_color
                         last_color += 1
                         colors[v] = last_color
-    return colors
+    for v in vert:
+        v.label = colors[v]
+        v.colornum = v.label
+    return graph
+
+
+with open('examplegraph2.gr') as f:
+    G = load_graph(f)
+
+K = color_refinement(G)
+
+with open('coloured.dot', 'w') as f:
+    write_dot(K,f)
+
+
+
 
 
 
