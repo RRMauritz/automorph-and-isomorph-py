e3de01d3a9e37977ee670ea0b1af20406813ae6d
s1750321 <t.e.pinke@student.utwente.nl>
2019-03-07 21:20:42 +0100

    fix bug in color_refinement and use argv for selecting graphs

--- color_refinement.py
+++ color_refinement.py
@@ -2,62 +2,67 @@ from graph import *
 from collections import Counter
 from graph_io import load_graph, write_dot
 from itertools import product
+from copy import deepcopy
 
 
-def color_refinement(G: "Graph", reset = True):
+def color_refinement(G: "Graph", reset_colors=True):
     # Initialize colors for every vertex
     colors = {}
-    if reset:
-        colors = {v: v.degree for v in G.vertices}
+    if reset_colors:
+        colors = {v.label: v.degree for v in G.vertices}
     else:
-        colors = {v: v.color for v in G.vertices}
+        colors = {v.label: v.color for v in G.vertices}
     # Find the highest color value
-    last_color = max([c for k, c in colors.items()])
+    #last_color = max([c for k, c in colors.items()])
     # Stores version of color configuration from previous loop iteration
     colors_old = {}
     # Keeps track of colors used for certain neighbourhoods
-    c_map = {}
+    for v in G.vertices:
+        v.colornum = colors[v.label]
 
+    c_map = {}
+    last_color = max([c for _, c in colors.items()])
     while not colors == colors_old:
         # Store last iteration
-        colors_old = colors.copy()
+        colors_old = deepcopy(colors)
 
         # Create coloring of vertices
         # Iterate over all vertices
         for u, v in product(G.vertices, G.vertices):
             # Check if colors were the same previously
-            if colors_old[u] == colors_old[v]:
+            if colors_old[u.label] == colors_old[v.label]:
                 # Get the colors of the neighbourhood
-                u_neighbours = Counter([colors_old[k] for k in u.neighbours])
-                v_neighbours = Counter([colors_old[k] for k in v.neighbours])
+                u_neighbours = Counter(
+                    [colors_old[k.label] for k in u.neighbours])
+                v_neighbours = Counter(
+                    [colors_old[k.label] for k in v.neighbours])
 
                 if u_neighbours == v_neighbours:
                     # If the neighbourhood color is not defined, add it to the dict
+                    # And keep it the same
                     if frozenset(u_neighbours.items()) not in c_map.keys():
-                        c_map[frozenset(u_neighbours.items())] = colors_old[u]
-
-                    # Assign colors to vertices
-                    colors[u] = c_map[frozenset(u_neighbours.items())]
-                    colors[v] = c_map[frozenset(v_neighbours.items())]
+                        c_map[frozenset(
+                            u_neighbours.items())] = colors_old[u.label]
                 else:
                     # If the two nodes don't share the same neighbourhood
                     # Make a new color for one of them
-                    last_color += 1
-                    c_map[frozenset(v_neighbours.items())] = last_color
+                    if frozenset(u_neighbours.items()) not in c_map.keys():
+                        last_color += 1
+                        c_map[frozenset(u_neighbours.items())] = last_color
+                    if frozenset(v_neighbours.items()) not in c_map.keys():
+                        last_color += 1
+                        c_map[frozenset(v_neighbours.items())] = last_color
+                    # Only recolor if the vertices are different
+                    if not u.label == v.label:
+                        colors[v.label] = c_map[frozenset(
+                            v_neighbours.items())]
+                        colors[u.label] = c_map[frozenset(
+                            u_neighbours.items())]
+                        u.colornum = colors[u.label]
+                        v.colornum = colors[v.label]
 
     # Make labels the color number and graph colorful
     for v in G.vertices:
-        v.color = colors[v]
+        v.color = colors[v.label]
         v.colornum = v.color
     return colors
-
-# Load graph
-with open('./colorref_smallexample_4_16.grl') as f:
-    G1 = load_graph(f)
-
-# Apply function
-color_refinement(G1)
-
-# Write the dot file
-with open('colored.dot', 'w') as f:
-    write_dot(G1, f)
--- isomorph.py
+++ isomorph.py
@@ -2,7 +2,7 @@ from graph import *
 from graph_io import *
 from collections import Counter
 from color_refinement import color_refinement
-from copy import deepcopy
+import sys
 
 
 def is_unbalanced(A, B):
@@ -18,6 +18,7 @@ def is_bijective(A, B):
 
 
 def count_isomorphisms(X: "Graph", Y: "Graph"):
+    global iterations
     # If the number of vertices or edges is different they cannot be
     # isomophic
     if not len(X.vertices) == len(Y.vertices) or not len(X.edges) == len(
@@ -27,16 +28,14 @@ def count_isomorphisms(X: "Graph", Y: "Graph"):
     # Create disjoint union
     U = X + Y
     # Apply color refinement
-    color_refinement(U, False)
+    color_refinement(U, reset_colors=False)
     # Split the union up again
     A, B = U.split_disjoint()
 
     # Check for unbalancy and bijectivity for early recursion exit
     if is_unbalanced(A, B):
-        print("unbalanced")
         return 0
     elif is_bijective(A, B):
-        print("bijective")
         return 1
 
     # Get the number of occurences for each color
@@ -45,36 +44,35 @@ def count_isomorphisms(X: "Graph", Y: "Graph"):
     # or equal to 2
     # This color occurs at least twice in both graphs A and B because
     # they are not unbalanced
-    color_class = [c for c, n in colors.items() if n >= 2][0]
+    color_class = [(c, n) for c, n in colors.items() if n >= 2][0]
     # Get a vertice that belongs to the color class
-    v = [v for v in A.vertices if v.color == color_class][0]
+    v = [v for v in A.vertices if v.color == color_class[0]][0]
 
     v.color = A.max_color + 1
     v.colornum = v.color
-    with open('colored.dot', 'w') as f:
-        write_dot(A + B, f)
 
     num = 0
-    for u in [k for k in B.vertices if k.color == color_class]:
+    for u in [k for k in B.vertices if k.color == color_class[0]]:
         # Save old color
         old_u_color = u.color
         # Make the vertices the same color
         u.color = v.color
         u.colornum = v.colornum
         # Recursion step
-        num += count_isomorphisms(A, B)
+        num += count_isomorphisms(A, B, debug=debug)
         # Change color back so another vertex can get colored
         u.color = old_u_color
         u.colornum = old_u_color
     return num
 
-with open('./colorref_smallexample_4_16.grl') as f:
-    graph_list = load_graph(f, read_list=True)
 
-A = graph_list[0][0]
-B = graph_list[0][1]
-U = A + B
-color_refinement(U)
-with open('colored.dot', 'w') as f:
-    write_dot(U, f)
-print(count_isomorphisms(A, B))
+if len(sys.argv) > 3:
+    with open(sys.argv[1]) as f:
+        graph_list = load_graph(f, read_list=True)
+
+    A = graph_list[0][int(sys.argv[2])]
+    B = graph_list[0][int(sys.argv[3])]
+    print(count_isomorphisms(A, B))
+else:
+    print("Need 3 arguments: %filename% %graph#1% %graph#2%")
+    print("The graph numbers refer to the indexes in the list of graphs")
