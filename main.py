f9a0c5797bc628d61f39d8bc7ad4d8d2ac1a8b6f
xynxynxyn <t.e.pinke@student.utwente.nl>
2019-04-09 16:48:14 +0200

    sick argument parser

--- main.py
+++ main.py
@@ -4,57 +4,68 @@ from graph_io_adj import load_graph_list
 from graph_lib import cycles_from_mapping
 import itertools as it
 import sys
+import argparse
 
 if __name__ == "__main__":
-    if sys.argv[1] == "-a" or sys.argv[1] == "--aut":
-        with open(sys.argv[2]) as f:
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    iso = group.add_argument_group()
+    aut = group.add_argument_group()
+
+    iso.add_argument("-i", "--iso", action="store_true")
+    iso.add_argument("-g", "--graph", nargs="*", type=int)
+    aut.add_argument("-a", "--aut", action="store_true")
+    parser.add_argument("path")
+    args = parser.parse_args()
+    print(args)
+    if args.aut:
+        with open(args.path) as f:
+            G = load_graph_list(f)
+        if args.graph:
+            G = [g for i, g in enumerate(G) if i in args.graph]
+
+        if args.graph:
+            zipper = args.graph
+        else:
+            zipper = range(len(G))
+        for i, g in zip(zipper, G):
+            print("[{}] {}".format(i, count_aut(g)))
+    elif args.iso:
+        with open(args.path) as f:
             G = load_graph_list(f)
 
-        handle_list = False
-        if len(sys.argv) >= 4:
-            if sys.argv[3] == "-l" or sys.argv[3] == "--list":
-                handle_list = True
-            else:
-                x = sys.argv[3]
-                G = G[int(sys.argv[3])]
+        if args.graph:
+            G = [g for i, g in enumerate(G) if i in args.graph]
+
+        pairs = list()
+        passed = set()
+
+        if args.graph:
+            zipper = args.graph
         else:
-            G = G[0]
-            x = 0
+            zipper = range(len(G))
+        for (i, a), (j, b) in it.product(zip(zipper, G),
+                                         zip(zipper, G)):
+            if i == j or i in passed or j in passed:
+                continue
 
-        if handle_list:
-            for i, g in enumerate(G):
-                print("[{}] {}".format(i, count_aut(g)))
-        else:
-            print("[{}] {}".format(x, count_aut(G)))
-    elif sys.argv[1] == "-i" or sys.argv[1] == "--iso":
-        with open(sys.argv[2]) as f:
-            G = load_graph_list(f)
+            if is_iso(a, b):
+                pairs.append([i, j])
+                passed.add(j)
 
-        handle_two = False
-        if len(sys.argv) >= 5:
-            G = [G[int(sys.argv[3])], G[int(sys.argv[4])]]
-            handle_two = True
-
-        if handle_two:
-            if is_iso(G[0], G[1]):
-                print("[{}] and [{}] are isomorphic".format(
-                    sys.argv[3], sys.argv[4]))
-            else:
-                print("[{}] and [{}] are not isomorphic".format(
-                    sys.argv[3], sys.argv[4]))
-        else:
-            pairs = list()
-            passed = set()
-
-            for (i, a), (j, b) in it.product(enumerate(G), enumerate(G)):
-                if i == j or i in passed or j in passed:
-                    continue
-
-                if is_iso(a, b):
-                    pairs.append([i, j])
-                    passed.add(j)
-
-            cycles = cycles_from_mapping(pairs)
-            
+        cycles = cycles_from_mapping(pairs)
+        single = set()
+        for i in zipper:
+            found = True
             for c in cycles:
-                print(c)
+                if i in c:
+                    found = False
+                    break
+            if found:
+                single.add(i)
+
+        for c in cycles:
+            print(c)
+
+        for i in single:
+            print("[{}]".format(i))
