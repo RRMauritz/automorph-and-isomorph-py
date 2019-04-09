acf7e541112826c5b3b65acfca68800055139a05
xynxynxyn <t.e.pinke@student.utwente.nl>
2019-04-09 17:03:21 +0200

    even better

--- main.py
+++ main.py
@@ -6,18 +6,44 @@ import itertools as it
 import sys
 import argparse
 
+
+def print_help():
+    print(
+        "Usage: python main.py graphs/graph.grl [--iso | --aut] {--graph 0 1 3 ...}"
+    )
+    print("[--iso]   Evaluate equivalence classes.")
+    print("[--aut]   Count automorphisms of graphs.")
+    print("{--graph} Indicate the indices of the graphs to be parsed.")
+    print("          If not specified every graph is parsed.")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group()
     iso = group.add_argument_group()
     aut = group.add_argument_group()
 
-    iso.add_argument("-i", "--iso", action="store_true")
+    iso.add_argument("-i",
+                     "--iso",
+                     action="store_true",
+                     help="Determine isomorphism equivalence classes")
     iso.add_argument("-g", "--graph", nargs="*", type=int)
-    aut.add_argument("-a", "--aut", action="store_true")
+    aut.add_argument("-a",
+                     "--aut",
+                     action="store_true",
+                     help="Calculate number of automorphisms")
     parser.add_argument("path")
-    args = parser.parse_args()
-    print(args)
+    try:
+        args = parser.parse_args()
+    except:
+        print_help()
+        parser.exit()
+
+    if args.iso and args.aut:
+        print("Cannot use --iso and --aut at the same time")
+        print_help()
+        parser.exit()
+
     if args.aut:
         with open(args.path) as f:
             G = load_graph_list(f)
@@ -25,9 +51,10 @@ if __name__ == "__main__":
             G = [g for i, g in enumerate(G) if i in args.graph]
 
         if args.graph:
-            zipper = args.graph
+            zipper = sorted(args.graph)
         else:
             zipper = range(len(G))
+
         for i, g in zip(zipper, G):
             print("[{}] {}".format(i, count_aut(g)))
     elif args.iso:
@@ -41,7 +68,7 @@ if __name__ == "__main__":
         passed = set()
 
         if args.graph:
-            zipper = args.graph
+            zipper = sorted(args.graph)
         else:
             zipper = range(len(G))
         for (i, a), (j, b) in it.product(zip(zipper, G), zip(zipper, G)):
